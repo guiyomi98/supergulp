df5eabaf08c29507be0c5ec92a06ea67c35580a9
guiyomi98 <guiyomi98@gmail.com>
2021-08-23 16:30:48 +0900

    Add files via upload
    initial

---
+++ gulpfile.babel.js
@@ -0,0 +1,75 @@
+'use strict';
+
+import gulp from "gulp"
+import pug from "gulp-pug"
+import del from "del"
+import webserver from "gulp-webserver"
+import image from "gulp-image"
+import autoprefixer from "gulp-autoprefixer"
+
+const sass = require('gulp-sass')(require('sass'));
+
+// directory
+const paths = {
+  html: {
+    src: "src/*.pug",
+    dist: "dist",
+    watch: "src/**/*.pug"
+  },
+  img: {
+    src: "src/images/*",
+    dist: "dist/assets/img"
+  }
+}
+
+// htmls
+const pugs = () => 
+  gulp
+    .src(paths.html.src)
+    .pipe(pug())
+    .pipe(gulp.dest(paths.html.dist))
+
+// images
+const imgs = () => 
+  gulp
+    .src(paths.img.src)
+    .pipe(image())
+    .pipe(gulp.dest(paths.img.dist))
+
+// scss
+const styles = () => 
+  gulp
+    .src("src/scss/styles.scss")
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest("dist/assets/stylesheets/styles.css"))
+
+
+// clean
+const clean = () => del(["dist"])
+
+// watch
+const watch = () => {
+  gulp.watch(paths.html.watch, pugs)
+  // gulp.watch(paths.img.watch, imgs)
+  gulp.watch('src/scss/*.scss', styles)
+}
+
+// webserver
+const server = () => 
+  gulp
+    .src("dist")
+    .pipe(webserver({
+      livereload: true,
+      open: true
+    }))
+
+
+const prepare = gulp.series([clean, imgs])
+
+const assets = gulp.series([pugs, styles])
+
+const post = gulp.series([server, watch])
+
+// develop oreder
+export const dev = gulp.series([assets, post])
